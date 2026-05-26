a49a866f90c89152a8be769a0fef28186eb51ea3
수연 <iioese109@gmail.com>
2026-05-26 17:32:57 +0900

    수정

--- api/models.py
+++ api/models.py
@@ -83,7 +83,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DB_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
